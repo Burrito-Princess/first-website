8e2b60de41464fc9e054630872013249b49cba85
Ross <burrito_princess_@Rosss-MacBook-Pro.local>
2022-09-13 11:34:16 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+var name1;
+var birthday;
+
+function discord (){
+    window.open(discord.gg/ENkHwfP9);
+}
+
+function getdata(){
+    name1= 
+    document.getElementById("name").value;
+    
+    birthday= 
+    document.getElementById("birthday").value;
+
+    console.log("getdata")
+
+}
+
+intervalID= setInterval(function(){
+    console.log(name1);
+    console.log(birthday);
+}, 1000);
